569d57b22e394d2b993d12abdd7771276217b5f7
Don Cote <mistercote@gmail.com>
2026-02-17 23:43:26 -0500

    workspaces

--- trigger.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@trigger.dev/sdk/v3'
-
-export default defineConfig({
-  project: 'proj_aeybunmgupltmhuulrbr',
-  dirs: ['./apps/web/trigger'],
-  maxDuration: 300, // 5 minutes
-})
